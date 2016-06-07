3fbcfc1c1f8b6f9f9e7819daac655a62f8caffb5
Sami Al-Jamal and Wolfgang Warneke <sami@>
2016-06-07 10:24:59 -0700

    Add conditionals to filter out improper input

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,20 @@ $(document).ready(function () {
   $("#counter").submit(function(event) {
     var numberMax = parseInt($('#numberMax').val());
     var interval = parseInt($('#interval').val());
+
+    if ( ($('#numberMax').val() === "") || ($('#interval').val() === "") ) {
+      alert('Hey! No empty input boxes, okay?');
+    }
+    if ( !numberMax || !interval  ) {
+      alert('Hey! Those don\'t look like numbers to me');
+    }
+    if ( (numberMax < 0) || (interval < 0)) {
+      alert('Guess what?  No negative numbers, please.');
+    }
+    if ( interval > numberMax ) {
+      alert('Interval cannot be larger than the number you are counting to.');
+    }
+
     for ( var i = interval; i <= numberMax; i += interval ) {
       $("#results").append('<li>'+i+'</li>');
     }
